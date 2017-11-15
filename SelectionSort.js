e90c9ffd96b843eaffadd7d244e6edc4fbe1d766
SHEFFmachine <sheff@sheffmachine.com>
2017-11-14 23:14:03 -0800

    selection sort done

--- SelectionSort.js
+++ SelectionSort.js
@@ -1,35 +1,44 @@
 class SelectionSort {
   run(inputArr) {
     const length = inputArr.length; //to make life easier
-
+    
     //The unsorted region starts at the length of the array, moves on by 1
     for (let i = 0; i < length - 1; i++) //This can run till length - 1 as another way to optimize
     {
       //Let's keep track of the lowest item's index by first setting it to i as a initial value.
       var lowestItemIndex = i;
-
+      
       //The loop that finds the lowest element in the unsorted region of the array.
-      for (let j = i + 1; j < length; j++) {
-        if (inputArr[j] < inputArr[lowestItemIndex]) {
+      for (let j = i + 1; j < length; j++)
+      {
+        if (inputArr[j] < inputArr[lowestItemIndex])
+        {
           lowestItemIndex = j; // current passes's lowest item
         }
       }
-
-      let key = inputArr[lowestItemIndex]; // keep track of the min key
-      while (lowestItemIndex > i) // while loop to shift items to the right
+      
+      //This is an optimization to prevent swapping if lowest index is equal to i.
+      //If i is equal to lowest item idex, no swap is necessary.
+      if (lowestItemIndex != i && inputArr[lowestItemIndex] != inputArr[i]) //don't swap same values
       {
-        inputArr[lowestItemIndex] = inputArr[lowestItemIndex - 1]; // actual shift to right
-        lowestItemIndex--; // move on till u hit i
+        /* =============== standard swap ================ */
+        // var temp = inputArr[lowestItemIndex];
+        // inputArr[lowestItemIndex] = inputArr[i];
+        // inputArr[i] = temp;
+        
+        /* =============== no extra var swap ================ */
+        // inputArr[lowestItemIndex] = inputArr[lowestItemIndex] + inputArr[i];
+        // inputArr[i] = inputArr[lowestItemIndex] - inputArr[i];
+        // inputArr[lowestItemIndex] = inputArr[lowestItemIndex] - inputArr[i];
+
+        /* =============== Elegant Swap ===================== */
+        [inputArr[lowestItemIndex], inputArr[i]] = [inputArr[i], inputArr[lowestItemIndex]];
       }
-
-      inputArr[i] = key; //place key in the new "opened up slot"
-
-
-    } //once we are done with this loop we have a table sorted array.
-
-    inputArr.forEach((item) => {
+    } //once we are done with this loop we have a sorted array.
+    
+    inputArr.forEach(item => {
       console.log(item);
-    })
+    });
   }
 }
 
--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+const StableSelectionSort = require('./StableSelectionSort');
 const SelectionSort = require('./SelectionSort');
+
 
 const selectionSort = new SelectionSort();
 selectionSort.run([23, 42, 4, 16, 8, 15]);
+
+const stableSelectionSort = new StableSelectionSort();
+stableSelectionSort.run([23, 42, 4, 16, 8, 15]);
