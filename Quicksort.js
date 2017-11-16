c79b08d54e484c9eb45ff7a5ef029ab99f528b21
SHEFFmachine <sheff@sheffmachine.com>
2017-11-15 22:24:16 -0800

    Quicksort

---
+++ Quicksort.js
@@ -0,0 +1,38 @@
+class Quicksort {
+  QuicksortWrapper (A, start, end) {
+    if (start < end) {
+      var partitionIndex = this.Partition(A, start, end); // we partition the array such that elements smaller are on the right and elements higher are on the left, and return pi
+      this.QuicksortWrapper(A, start, partitionIndex - 1); // Run recursion on the left side of the array
+      this.QuicksortWrapper(A, partitionIndex + 1, end); // Run recursion on the right side of the array
+    }
+  }
+  
+  // Partition logic
+  Partition(A, start, end) {
+    var pivot = A[end]; // pick a pivort as the last element
+    var i = start - 1; // i is a wall here, so at first the wall is to the irght of the array at i = -1
+    for (let j = start; j < end; j++)
+    {
+      if (A[j] < pivot) // let's compare the the jth element in the array with our pivot, to see if it's smaller then pivot
+      {
+        i++; // move the wall to the right, and by doing it create a selector for element to right of wall where it was before
+        //swap array i with the first element to the right of the wall
+        // var tempj = A[j];
+        // A[j] = A[i];
+        // A[i] = tempj;
+
+        [A[j], A[i]] = [A[i], A[j]]
+      }
+    }
+    
+    //Once we reach the end of the array, we want to swap the last element (pivot) with the element to the right of the wall (i+1)
+    // var tempi = A[i + 1];
+    // A[i + 1] = A[end];
+    // A[end] = tempi;
+    [A[i + 1], A[end]] = [A[end], A[i + 1]]
+    
+    return i + 1; //Return the position of the wall after the swap.
+  }
+}
+
+module.exports = Quicksort;
--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const StableSelectionSort = require('./StableSelectionSort');
 const SelectionSort = require('./SelectionSort');
 const InsertionSort = require('./InsertionSort');
+const QuickSort = require('./Quicksort');
 
-const insertionSort = new InsertionSort();
-insertionSort.run([23, 42, 4, 16, 8, 15]);
+const quickSort = new QuickSort();
+var inputArr = [7, 2, 1, 6, 8, 5, 3, 4];
+quickSort.QuicksortWrapper(inputArr, 0, inputArr.length - 1);
+inputArr.forEach((item) => {
+  console.log(item);
+});
 
-const selectionSort = new SelectionSort();
-selectionSort.run([23, 42, 4, 16, 8, 15]);
+// const insertionSort = new InsertionSort();
+// insertionSort.run([23, 42, 4, 16, 8, 15]);
 
-const stableSelectionSort = new StableSelectionSort();
-stableSelectionSort.run([23, 42, 4, 16, 8, 15]);
+// const selectionSort = new SelectionSort();
+// selectionSort.run([23, 42, 4, 16, 8, 15]);
+
+// const stableSelectionSort = new StableSelectionSort();
+// stableSelectionSort.run([23, 42, 4, 16, 8, 15]);
